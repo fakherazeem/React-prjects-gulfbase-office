5546040de0b294affeb569f039edcf3efca8bcce
fakherazeem <fakherazeem1@gmail.com>
2026-03-06 10:12:15 +0500

    ok

--- atomic-bomb/src/main.jsx
+++ atomic-bomb/src/main.jsx
@@ -1,8 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// point directly at the JSX version so we don’t accidentally load the
-// other `App.js` file (which also imports faker, a package not installed
-// in this project). Using the extension avoids Vite/Node resolution ambiguity.
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
